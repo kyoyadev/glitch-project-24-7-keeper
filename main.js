6c265d088d677ec47f9bc0cfd42e7f43fcd4d86d
KYOYA <63583961+kyoyawashere@users.noreply.github.com>
2020-08-24 01:04:56 +0300

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const { get } = require('node-superfetch') // DO NOT DELETE.
 
 setInterval(()=>{// DO NOT DELETE.
 fetch(`https://yourglitchproject.glitch.me/`) // CHANGE TO YOUR PROJECT NAME
-get('https://yourglitchproject.glitch.me/`)// CHANGE TO YOUR PROJECT NAME
+get(`https://yourglitchproject.glitch.me/`)// CHANGE TO YOUR PROJECT NAME
 console.log('Pong!')//YOU CAN DELETE THIS , IF YOU WANT.
 }, 60000)// DO NOT DELETE OR CHANGE.
 
